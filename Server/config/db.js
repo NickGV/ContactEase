712be59c147d3d5d32d5168e23dd55cb2dbb67a6
Nick <nickgomvelez@gmail.com>
2025-02-11 10:26:23 -0500

    fix connection to the database

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected");
+    console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
-    console.error(error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
--- Server/server.js
+++ Server/server.js
@@ -1,38 +1,20 @@
+require("dotenv").config();
 const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 const connectDB = require("./config/db");
 
-dotenv.config();
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(express.json());
 connectDB();
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/contacts", contactRoutes);
 
-io.on("connection", (socket) => {
-  console.log("Usuario conectado");
-
-  socket.on("sendMessage", (message) => {
-    io.emit("receiveMessage", message);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Usuario desconectado");
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
