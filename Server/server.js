809c318cac050e953e539d2422cca04700fd84dc
Nick <nickgomvelez@gmail.com>
2025-02-13 11:34:53 -0500

    add error handling middleware and update socket configuration import

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,8 @@ const authRoutes = require("./routes/authRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 const connectDB = require("./config/db");
-const configureSocket = require("./config/socket");
+const configureSocket = require("./config/socketConfig");
+const errorMiddleware = require("./middlewares/errorMiddleware");
 
 const app = express();
 const server = http.createServer(app);
@@ -19,6 +20,8 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/contacts", contactRoutes);
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
