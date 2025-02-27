d4ed133af9c3551c94980daaab93b0728b0f69f1
Nick <nickgomvelez@gmail.com>
2025-02-27 17:29:46 -0500

    refactor: remove ChatContactItem component; integrate its functionality directly into ChatSidebar

--- Client/src/components/ChatSidebar.jsx
+++ Client/src/components/ChatSidebar.jsx
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ContactsContext } from '../context/ContactsContext'
-import { faArrowLeft, faUserPlus } from '@fortawesome/free-solid-svg-icons'
-import { ChatContactItem } from './ChatContactItem'
-import { useContext, useState } from 'react'
+import { faArrowLeft, faUser, faUserPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useContext } from 'react'
 import { SearchBar } from './SearchBar'
 
 export const ChatSidebar = () => {
-  const { contacts, searchResultsFound, searchTerm, setSearchTerm, selectedContact } = useContext(ContactsContext)
+  const {
+    contacts, searchResultsFound, searchTerm, setSearchTerm, selectedContact, handleSelectedContact,
+    deleteContact
+  } = useContext(ContactsContext)
 
   const filteredContacts = contacts.filter(contact => {
     if (searchTerm.trim() === '') {
@@ -18,6 +20,10 @@ export const ChatSidebar = () => {
         value.toLowerCase().includes(searchTerm.toLowerCase())
     )
   })
+  const handleclick = (e, contact) => {
+    e.stopPropagation()
+    handleSelectedContact(contact)
+  }
 
   return (
     <div className='m-w-full w-11/12  overflow-x-auto md:overflow-y-auto pt-1 px-2 '>
@@ -58,14 +64,33 @@ export const ChatSidebar = () => {
           <ul className={`m-w-full flex md:grid ${selectedContact ? 'md:grid-cols-1' : 'md:grid-cols-2 lg:grid-cols-3'} gap-4`}>
             {filteredContacts.map((contact) => (
               <li className="md:border-b" key={contact.id}>
-                <ChatContactItem
-                  id={contact.id}
-                  name={contact.name}
-                  email={contact.email}
-                  phoneNumber={contact.phoneNumber}
-                  isFavorite={contact.isFavorite}
-                  notes={contact.notes}
-                />
+                <div
+                      className={`relative flex gap-3 md:justify-between p-4 md:bg-black-bg hover:cursor-pointer w-20 md:w-full rounded-lg ${
+                        selectedContact?.id === contact.id ? 'md:bg-slate-700' : ''
+                      }`}
+                      onClick={(e) => handleclick(e, contact)}
+                    >
+                      <div className="flex flex-col md:flex-row md:gap-3 items-center w-full">
+                        <div className="w-15 flex justify-center items-center text-3xl md:text-5xl text-white-btn-text bg-black-bg md:bg-transparent border rounded-full h-16 w-16 md:h-14 md:w-14 p-4">
+                          <FontAwesomeIcon icon={faUser} className="w-6" />
+                        </div>
+                        <div className="text-center md:text-left">
+                          <p className="font-bold text-white text-sm md:text-lg">
+                            {name}
+                          </p>
+                          <p className="hidden md:block md:text-md lg:text-lg">{contact.phoneNumber}</p>
+                          <a href={`mailto:${contact.email}`} className="hidden md:block text-sm xl:text-lg text-link underline">
+                            {contact.email}
+                          </a>
+                        </div>
+                      </div>
+                      <button
+                        className="hidden md:block absolute text-xl top-0 right-0 mr-2 text-gray-300 hover:text-red-500 hover:scale-110 transition-all"
+                        onClick={() => deleteContact(id)}
+                      >
+                        <FontAwesomeIcon icon={faXmark} />
+                      </button>
+                    </div>
               </li>
             ))}
           </ul>
