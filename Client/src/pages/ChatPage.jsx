1078f565579db6b15cfc8582241a7a01d160f18d
Nick <nickgomvelez@gmail.com>
2025-04-08 21:01:37 -0500

    refactor: enhance styling and layout in ChatSidebar and ContactChatSelector components for improved responsiveness; update button sizes and text for better user experience

--- Client/src/pages/ChatPage.jsx
+++ Client/src/pages/ChatPage.jsx
@@ -5,12 +5,12 @@ import useChat from '../hooks/useChat'
 export const ChatPage = () => {
   const { selectedChat } = useChat()
   return (
-    <main className="flex flex-col md:flex-row h-4/6 md:gap-2 md:h-5/6 xl:h-87 m-4">
-        <div className={`w-full ${selectedChat ? 'md:w-1/3' : 'md:w-full'} h-36 md:h-95 lg:h-full md:flex justify-center`}>
+    <main className="flex flex-col md:flex-row h-[calc(100vh-6rem)] overflow-hidden mx-2 sm:mx-3 md:mx-4 my-2 sm:my-3 md:my-4">
+        <div className={`w-full ${selectedChat ? 'h-1/3 sm:h-2/5 md:h-full md:w-1/3 lg:w-1/4' : 'h-full'} transition-all duration-300`}>
             <ChatSidebar />
         </div>
         {selectedChat && (
-          <div className="w-full h-5/6 md:h-95 lg:h-full flex flex-col flex-1 item-center ">
+          <div className="w-full h-2/3 sm:h-3/5 md:h-full md:flex-1 flex flex-col transition-all duration-300">
             <ChatWindow/>
           </div>
         )}
